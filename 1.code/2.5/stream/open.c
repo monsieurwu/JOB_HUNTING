3746c92f0f0ce3423cb968f639f81fde8f1fbf75
wu.jianwei <wu.jianwei@byd.com>
2024-08-13 17:46:49 +0200

    update doc

---
+++ 1.code/2.5/stream/open.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main(){
+    FILE *stream1 = NULL;
+    FILE *stream2 = NULL;
+
+    stream1 = fopen("1.txt", "a+");
+    if(stream1 == NULL)
+    {
+        printf("Error opening file 1.txt\n");
+        return 1;
+    }else {
+        printf("File 1.txt opened successfully.\n");
+    }
+
+
+
+    if(fclose(stream1) == EOF) {
+        printf("Error closing file 1.txt\n");
+        return 1;
+
+    } 
+    return 0;
+}
