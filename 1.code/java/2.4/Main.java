69799483c8b4e6740419896b82e45dac3567470c
wu.jianwei <wu.jianwei@byd.com>
2024-10-23 17:38:20 +0200

    oop

---
+++ 1.code/java/2.4/Main.java
@@ -0,0 +1,12 @@
+// for each
+public class Main {
+    public static void main(String[] args) {
+        int[] ns = { 1, 4, 9, 16, 25 };
+        int sum = 0;
+        for (int n : ns) {
+            // TODO
+            sum += n;
+        }
+        System.out.println(sum); // 55
+    }
+}
---
+++ 1.code/java/2.5/Main.java
@@ -0,0 +1,18 @@
+// City
+public class Main {
+    public static void main(String[] args) {
+        City bj = new City();
+        bj.name = "Beijing";
+        bj.latitude = 39.903;
+        bj.longitude = 116.401;
+        System.out.println(bj.name);
+        System.out.println("location: " + bj.latitude + ", " + bj.longitude);
+    }
+}
+
+class City {
+    String name;
+    double latitude;
+    double longitude;
+    
+}
